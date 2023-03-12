d753aa32a52ef4eb602acdb4a4e6f7078668b89c
TaxMachine <tax.machine348@gmail.com>
2023-03-12 17:12:54 -0400

    h

--- spammer.nim
+++ spammer.nim
@@ -13,7 +13,7 @@ client.headers = newHttpHeaders({
     "User-Agent": "WaifuWare Webhook Spammer"
 })
 
-proc clear: void {.compileTime.} = discard execShellCmd(when defined(windows): "cls" else: "clear")
+proc clear: void = discard execShellCmd(when defined(windows): "cls" else: "clear")
 
 proc printDashboard(res: JsonNode, spammed: int): void =
     clear()
@@ -31,12 +31,24 @@ proc printDashboard(res: JsonNode, spammed: int): void =
     echo "-----------------------------------------------"
 
 proc verifyWebhook(webhook: string): bool =
+    ## Verify if the given webhook exists
     let response = client.request(webhook, HttpGet)
-    echo response.status
     return response.status == $Http200
 
+proc sendWebhook(webhook: string, data: JsonNode): bool =
+    ## Send a given message to a give webhook
+    let response = client.request(webhook, HttpPost, $data)
+    return response.status == $Http200 or response.status == $Http204
+
+proc deleteWebhook(webhook: string): bool =
+    ## Deletes a given webhook and sends a delete message to the webhook before deleting it
+    ## - Raise Exception if the webhook doesnt exist or if the message fails to send
+    if not verifyWebhook(webhook): raise newException(Exception, "Webhook doesn't exist")
+    if not sendWebhook(webhook, config["deleteMessage"]): raise newException(Exception, "There was an error while sending the delete message")
+    let response = client.request(webhook, HttpDelete)
+    return response.status == $Http204 or response.status == $Http200
+
 when isMainModule:
-    
     var 
         webhook = commandLineParams()[0]
         res: JsonNode
@@ -52,7 +64,25 @@ when isMainModule:
     else:
         echo "[-] Webhook is invalid"
         quit(0)
-    
+    var choice: string
+    echo "[1] - Spam the webhook"
+    echo "[2] - Delete the webhook"
+    while true:
+        let ok = readLineFromStdin("[1/2] - ", choice)
+        if not ok: break
+        case choice:
+        of "1":
+            break
+        of "2":
+            try:
+                if deleteWebhook(webhook):
+                    echo "[+] Webhook Successfully deleted"
+                else:
+                    echo "[-] There was an error while trying to delete the webhook"
+            except Exception as e:
+                echo e.msg
+            quit(0)
+
     while true:
         let response = client.request(webhook, HttpPost, $body)
         case response.status:
@@ -63,7 +93,7 @@ when isMainModule:
                 printDashboard(res, spammed)
                 inc(spammed)
             of $Http404:
-                echo "[-] Webhook get deleted"
+                echo "[-] Webhook got deleted"
                 quit(0)
             of $Http429:
                 echo "[-] Rate limited, waiting 5 seconds"
@@ -73,4 +103,4 @@ when isMainModule:
                 echo response.status
                 echo response.body
                 quit(0)
-        #sleep(1500)
+        sleep(1500)
